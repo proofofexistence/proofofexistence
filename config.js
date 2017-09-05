ed54cb042678f35ed65efc6ded3f05f4af5ab728
Christopher Adams <christopher@poex.io>
2017-09-05 17:51:08 +0800

    Add function to normalize port from config

--- config.js
+++ config.js
@@ -36,6 +36,25 @@ var networkName = bitcore.Networks.defaultNetwork.name === 'testnet' ? 'test3' :
 
 var DEBUG = false
 
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
 
 export {
   BLOCKCYPHER_TOKEN,
