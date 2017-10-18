cff2752adf4689553d36ebe6fd488c309f61232b
Christopher Adams <christopher@poex.io>
2017-10-18 21:16:14 +0800

    Resubmit to poex.io

--- resubmit.sh
+++ resubmit.sh
@@ -3,4 +3,4 @@ echo "address: $1 tx: $2"
 
 curl https://api.blockcypher.com/v1/btc/main/txs/$2 > data.json
 
-curl -X 'POST' https://proofofexistence.com/unconfirmed/UsblXqKxH8wzEdFPRUwru2xqCztrhJcJrxQWTR9fLI/$1 -H 'Content-type: application/json' -d '@data.json'
+curl -X 'POST' https://poex.io/unconfirmed/UsblXqKxH8wzEdFPRUwru2xqCztrhJcJrxQWTR9fLI/$1 -H 'Content-type: application/json' -d '@data.json'
