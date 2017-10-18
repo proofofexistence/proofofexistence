b2c31652b2f6e98f7841bfe6a484b17e3e230d20
Christopher Adams <christopher@poex.io>
2017-10-18 21:33:43 +0800

    Increase txs limit in resubmit script

--- resubmit.sh
+++ resubmit.sh
@@ -1,6 +1,6 @@
 
 echo "address: $1 tx: $2"
 
-curl https://api.blockcypher.com/v1/btc/main/txs/$2 > data.json
+curl https://api.blockcypher.com/v1/btc/main/txs/$2?limit=9999 > data.json
 
 curl -X 'POST' https://poex.io/unconfirmed/UsblXqKxH8wzEdFPRUwru2xqCztrhJcJrxQWTR9fLI/$1 -H 'Content-type: application/json' -d '@data.json'
