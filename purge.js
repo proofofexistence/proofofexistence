04fd250c5f408b3084848937efa377542b4c6928
Christopher Adams <christopher@poex.io>
2017-11-16 16:54:32 +0800

    Convert purge script
    Use native node features, clean up formatting

--- purge.js
+++ purge.js
@@ -1,36 +1,28 @@
+'use strict'
 
-import bcypher from 'blockcypher'
-import {
-  BLOCKCYPHER_TOKEN,
-  MAGIC_NUMBER,
-  BASE_PRIVATE_KEY,
-  TARGET_PAYMENT_ADDRESS,
-  SERVER_URL,
-  networkName,
-  DEBUG
-} from './lib/config'
+var bcypher = require('blockcypher')
+var config = require('./lib/config')
 
-
-var MAX_HOOKS = 120;
-var bcapi = new bcypher('btc', networkName, BLOCKCYPHER_TOKEN);
-function printResponse(err, data) {
+var MAX_HOOKS = 120
+var BCypher = bcypher
+var bcapi = new BCypher('btc', config.networkName, config.BLOCKCYPHER_TOKEN)
+function printResponse (err, data) {
   if (err !== null) {
-    console.log(err);
+    console.log(err)
   } else {
-    console.log(data);
+    console.log(data)
   }
 }
 
-bcapi.listHooks(function(err, data) {
+bcapi.listHooks(function (err, data) {
   if (err !== null) {
-    console.log(err);
+    console.log(err)
   } else {
-    console.log('found',data.length, 'hooks')
+    console.log('found', data.length, 'hooks')
     while (data.length > MAX_HOOKS) {
-      var hook = data.shift();
+      var hook = data.shift()
       console.log('deleting', hook.id)
       bcapi.delHook(hook.id, printResponse)
     }
   }
-});
-
+})
