194739db11c9685966ed4d621c1541d2f8b70bb3
Christopher Adams <christopher@poex.io>
2017-12-22 14:50:42 +0800

    Test 200 status of confirmed and unconfirmed

--- test/server_spec.js
+++ test/server_spec.js
@@ -9,6 +9,30 @@ chai.use(chaiHttp)
 const server = require('../lib/server')
 const request = chai.request(server)
 
+describe('/GET latest unconfirmed', () => {
+  it('it should GET the latest unconfirmed', (done) => {
+    request
+      .get('/api/internal/latest/unconfirmed')
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res).to.have.status(200)
+        done()
+      })
+  })
+})
+
+describe('/GET latest confirmed', () => {
+  it('it should GET the latest confirmed', (done) => {
+    request
+      .get('/api/internal/latest/confirmed')
+      .end((err, res) => {
+        expect(err).to.be.null
+        expect(res).to.have.status(200)
+        done()
+      })
+  })
+})
+
 after(() => {
   server.stop()
 })
