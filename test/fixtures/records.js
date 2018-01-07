c8f683b07741c6c1cd8dd0f8f13738d75c2a77b1
Christopher Adams <christopher@poex.io>
2018-01-07 20:16:27 +0800

    Add records fixtures

---
+++ test/fixtures/records.js
@@ -0,0 +1,15 @@
+var digest = "15db6dbff590000ea13246e1c166802b690663c4e0635bfca78049d5a8762832"
+var address = "mhtW5ExcDL2VDEgH9tWPXrarueR7aP8BSk"
+
+module.exports = {
+  address: address,
+  digest: digest,
+  document: {
+    digest: digest,
+    path: "m/759030539/99107629/1105160692",
+    payment_address: address,
+    pending: true,
+    timestamp: "2018-01-07T07:51:00.946Z",
+    fee: null
+  }
+}
