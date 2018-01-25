ce9da367247cc002424bab4bb0b8b30a2271f402
Christopher Adams <christopher@poex.io>
2018-01-25 12:51:18 +0800

    Switch to paper-dashboard npm package

--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -3,7 +3,7 @@ mkdir -p public/vendor
 cp -R node_modules/jquery/dist public/vendor/jquery
 cp -R node_modules/jquery-form/ public/vendor/jquery-form
 cp -R node_modules/bootstrap/dist/ public/vendor/bootstrap
-cp -R assets/vendor/paper-dashboard/assets public/vendor/paper-dashboard
+cp -R node_modules/paper-dashboard/assets/ public/vendor/paper-dashboard
 cp -R node_modules/sprintf-js/dist public/vendor/sprintf-js
 cp -R node_modules/openpgp/dist public/vendor/openpgp
 cp -R node_modules/qrcodejs public/vendor/qrcodejs
