870059ed3b9ec7471998422b35ac79bd5a53a070
Christopher Adams <christopher@poex.io>
2018-02-06 16:54:34 +0800

    Increase estimate of OP_RETURN size

--- test/core_spec.js
+++ test/core_spec.js
@@ -10,14 +10,14 @@ describe('estimate docproof fees', () => {
   it('it should estimate a fee based on fee per kb', (done) => {
     feePerKb = 100000
     docproofFee = docproof.estimateFee(feePerKb)
-    expect(docproofFee).to.equal(23400)
+    expect(docproofFee).to.equal(24200)
     done()
   })
 
   it('it should estimate a fee based on fee per kb, with a multiplier', (done) => {
     feePerKb = 124099
     docproofFee = docproof.estimateFee(feePerKb, 4)
-    expect(docproofFee).to.equal(116157)
+    expect(docproofFee).to.equal(120128)
     done()
   })
 })
