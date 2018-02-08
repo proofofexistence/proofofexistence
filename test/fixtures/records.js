368a9414116d8ca27c9e4a1e20ad455bb19e9e16
Christopher Adams <christopher@poex.io>
2018-02-08 12:25:59 +0800

    Update fixture fee

--- test/fixtures/records.js
+++ test/fixtures/records.js
@@ -11,7 +11,7 @@ module.exports = {
     payment_address: address,
     pending: true,
     timestamp: timestamp,
-    fee: 113435
+    fee: 120128
   },
   unconfirmed: {
     digest: digest,
@@ -19,7 +19,7 @@ module.exports = {
     pending: true,
     timestamp: timestamp,
     feePerKilobyte: 124099,
-    fee: 113435
+    fee: 120128
   },
   confirmed: {
     digest: '29872ac22b737be58a0fcf82921c9d8b0cb7aa8f6d8e68b08663014d7d8a0449',
@@ -27,7 +27,7 @@ module.exports = {
     pending: false,
     timestamp: timestamp,
     feePerKilobyte: 124099,
-    fee: 113435,
+    fee: 120128,
     tx: 'b4d42409fefd3286e4f4f13e8e65caa525eaa3251ddafa0b9902ef7a26239816',
     txstamp: '2018-01-18T06:29:13.525Z',
     blockstamp: '2018-01-18T06:48:39Z'
