6be4085a36703b96f3c131d362225db59eff2be7
Christopher Adams <christopher@poex.io>
2018-02-26 18:46:26 +0800

    Switch purge script to blockcypher client module

--- purge.js
+++ purge.js
@@ -1,30 +1,18 @@
 'use strict'
 
-var bcypher = require('blockcypher')
+var bcapi = require('./lib/clients/blockcypher')
 var config = require('config')
 
 console.log(config.get('networkName'))
 
 var MAX_HOOKS = 120
-var BCypher = bcypher
-var bcapi = new BCypher('btc', config.get('networkName'), config.get('services.blockcypher.token'))
-function printResponse (err, data) {
-  if (err !== null) {
-    console.log(err)
-  } else {
-    console.log(data)
-  }
-}
 
-bcapi.listHooks(function (err, data) {
-  if (err !== null) {
-    console.log(err)
-  } else {
+bcapi.listHooks()
+  .then(data => {
     console.log('found', data.length, 'hooks')
     while (data.length > MAX_HOOKS) {
       var hook = data.shift()
       console.log('deleting', hook.id)
       bcapi.delHook(hook.id, printResponse)
     }
-  }
-})
+  }).catch(e => { console.log(e.message) })
