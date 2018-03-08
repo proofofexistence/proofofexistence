e0337f96db09e32ade253db382fc280dc5040a15
Christopher Adams <christopher@poex.io>
2018-03-08 14:19:26 +0800

    Fix and test status query route

--- test/server_spec.js
+++ test/server_spec.js
@@ -149,6 +149,33 @@ describe('register a document', () => {
       })
   })
 
+  it('it should query a status', (done) => {
+    db.batch()
+      .put(`map-${digest}`, address)
+      .put(address, JSON.stringify(document))
+      .write(() => {
+        request
+          .get(`/api/v1/status?d=${digest}`)
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(res).to.be.json
+
+            status = res.body
+            expect(status.success).to.equal(true)
+            expect(status.pending).to.equal(true)
+            expect(status.digest).to.equal(digest)
+            expect(status.payment_address).to.equal(address)
+            expect(status.price).to.equal(expected_price_satoshi)
+            expect(status.network).to.equal(expected_network)
+            expect(status.timestamp).to.be.a('string')
+            expect(status.txstamp).to.be.a('string')
+            expect(status.blockstamp).to.be.a('string')
+            done()
+          })
+      })
+  })
+
   it('it should handle a missing status', (done) => {
     request
       .get(`/api/v1/status/${digest}`)
