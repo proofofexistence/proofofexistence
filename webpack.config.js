fa15b14d08e311165f9697543d33c1c6cc674ec0
Clement Renaud <clement.renaud@gmail.com>
2018-04-29 18:16:47 +0200

    webpack prod build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,47 @@
-const webpack = require('webpack');
-const path = require('path');
-const debug = require('debug');
+const webpack = require('webpack')
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const BUILD_DIR = path.resolve(__dirname, 'ui/public/dist');
-const APP_DIR = path.resolve(__dirname, 'ui/app');
-const SCSS_DIR = path.resolve(__dirname, 'ui/scss');
-const SRC = path.resolve(__dirname, 'ui');
+const BUILD_DIR = path.resolve(__dirname, 'ui/public/dist')
+const APP_DIR = path.resolve(__dirname, 'ui/app')
+const SCSS_DIR = path.resolve(__dirname, 'ui/scss')
+const SRC = path.resolve(__dirname, 'ui')
 
-var config = {
+const envPlugin = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
+})
+
+const prod = process.env.NODE_ENV === 'production'
+
+
+const plugins = prod ?
+  [
+    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js','common.js'),
+    // new webpack.optimize.DedupePlugin(),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: { warnings: true }
+      }
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.LimitChunkCountPlugin({maxChunks: 15}),
+    new webpack.optimize.MinChunkSizePlugin({minChunkSize: 10000}),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    // new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /fr/),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    envPlugin
+  ]
+  :
+  []
+
+const devtool = prod ? 'cheap-module-source-map' : 'inline-sourcemap'
+const watch = !prod
+
+const config = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : false,
+  devtool: devtool,
   entry: APP_DIR + '/index.jsx',
-  watch: true,
+  watch: watch,
   output: {
     path: BUILD_DIR,
     filename: "client.min.js"
@@ -49,22 +79,7 @@ var config = {
       }
     ]
   },
-  devServer: {
-    host: 'localhost', // Defaults to `localhost`
-    port: 3000, // Defaults to 8080,
-    proxy: {
-      '^/api/*': {
-        target: 'http://localhost:3000/',
-        secure: false
-      }
-    }
-  },
-
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+  plugins: plugins
 }
 
 module.exports = config;
