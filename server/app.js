49536c53209997660440322d8c00c45c31180200
Clement Renaud <clement.renaud@gmail.com>
2018-04-29 22:49:58 +0200

    console branding !

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ var server = app.listen(config.get('app.port'), function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Proof of Existence listening at http://%s:%s', host, port)
 })
 
 const stop = () => {
