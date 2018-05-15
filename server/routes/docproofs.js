8d7d60234e455b5e72b3817eaa8ff0340bd26778
Christopher Adams <christopher@poex.io>
2018-05-15 17:40:41 +0800

    Look up txs data for docproofs

--- server/routes/docproofs.js
+++ server/routes/docproofs.js
@@ -1,6 +1,8 @@
 const core = require('../../lib/core')
+const Insight = require('../../lib/clients/insight')
 const SpecialOps = require('../../lib/clients/specialops')
 
+const insight = new Insight()
 const specialops = new SpecialOps()
 
 /**
@@ -9,12 +11,31 @@ const specialops = new SpecialOps()
 
 function docproofs (req, res) {
   const hash = req.params.hash
+  let docproofs = {}
 
   if (core.docproof.isValidDigest(hash)) {
     // TODO: add pagination query for additional data.
     specialops.getDocproofs(hash, {limit: 100})
       .then(results => {
-        res.json(results)
+        docproofs = results
+        return getTxs(results.items)
+      })
+      .then(txs => {
+        const items = txs.map(tx => {
+          return {
+            blockhash: tx.blockhash,
+            blockheight: tx.blockheight,
+            blocktime: tx.blocktime,
+            confirmations: tx.confirmations,
+            txid: tx.txid
+          }
+        }).sort((a, b) => {
+          return a.blockheight > b.blockheight
+        })
+
+        docproofs.items = items
+
+        return res.json(docproofs)
       }).catch(error => {
         console.log(error.message)
 
@@ -25,6 +46,14 @@ function docproofs (req, res) {
       reason: 'Invalid `hash` field'
     })
   }
+}
+
+async function getTxs (docproofs) {
+  const getTxs = docproofs.map(docproof => {
+    return insight.getTx(docproof.txid)
+  })
+
+  return Promise.all(getTxs)
 }
 
 module.exports = {
