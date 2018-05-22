74a55bc7f2a1c7960700991fc23603a81e2e46dc
Clement Renaud <clement.renaud@gmail.com>
2018-05-22 12:23:04 +0200

    update index to use promise

--- ui/app/index.jsx
+++ ui/app/index.jsx
@@ -9,28 +9,43 @@ import Error404 from './pages/Error404.jsx'
 
 import registerServiceWorker from './registerServiceWorker'
 
-// get config as starter
-api.getConfig(config => {
-  // config and global props to pass down to children
-  const generalProps = { ...config }
+import { getConfig } from "@proofofexistence/api-client"
 
-  ReactDOM.render(
-    <Router>
-      <App {...generalProps}>
-        <Switch>
-          <Route exact path='/' component={props =>
-            <Home {...generalProps} {...props} />
-            } />
-          <Route path='/detail/:hash' component={props =>
-            <Detail {...generalProps} {...props} />
-            } />
-          <Route component={Error404} />
-        </Switch>
-      </App>
-    </Router>
-    ,
-    document.getElementById('root')
-  )
-})
+// get config as starter
+getConfig()
+  .then( response => {
+    let config =response.data
+
+    // config and global props to pass down to children
+    const generalProps = { ...config }
+
+    ReactDOM.render(
+      <Router>
+        <App {...generalProps}>
+          <Switch>
+            <Route exact path='/' component={props =>
+              <Home {...generalProps} {...props} />
+              } />
+            <Route path='/detail/:hash' component={props =>
+              <Detail {...generalProps} {...props} />
+              } />
+            <Route component={Error404} />
+          </Switch>
+        </App>
+      </Router>
+      ,
+      document.getElementById('root')
+    )
+  })
+  .catch( error => {
+    console.log(error);
+    ReactDOM.render(
+      <p>
+        Error fetching the config file...
+      </p>
+      ,
+      document.getElementById('root')
+    )
+  })
 
 registerServiceWorker()
