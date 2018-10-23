bafb5f10759007b88fddec71a39820057ca407fc
qinfengling <Fengling.Qin@gmail.com>
2018-10-23 11:31:10 +0800

    Add transaction detail display for BTC mainnet

--- ui/app/components/status/Confirming.jsx
+++ ui/app/components/status/Confirming.jsx
@@ -11,8 +11,17 @@ const Confirming = ({
     </p>
     <ul class='list-group list-group-flush'>
       <li class='list-group-item'>
-        {tx}
-        <br />
+        <span class='badge badge-light'>Transaction ID: </span>{tx}
+        <br/>
+      </li>
+      {/* NOTE: Only support btc mainnet */}
+      <li class='list-group-item'>
+        <span class='badge badge-light'>Transaction Detail: </span>
+        <a class="btn btn-primary" target='_blank' style={{ marginLeft: '.8em' }} href={`https://insight.bitpay.com/tx/${tx}`}>insight</a>
+        <a class="btn btn-primary" target='_blank' style={{ marginLeft: '.8em' }} href={`https://www.smartbit.com.au/tx/${tx}`}>smartbit</a>
+        <a class="btn btn-primary" target='_blank' style={{ marginLeft: '.8em' }} href={`https://www.blockchain.com/btc/tx/${tx}`}>BLOCKCHAIN</a>
+        <a class="btn btn-primary" target='_blank' style={{ marginLeft: '.8em' }} href={`https://live.blockcypher.com/btc/tx/${tx}`}>BLOCKCYPHER</a>
+        <br/>
       </li>
     </ul>
     <div class='card-body'>
