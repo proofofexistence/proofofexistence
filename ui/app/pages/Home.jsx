dd6d015744fccbaa78951aefb1587b0c2c5047f4
Clement Renaud <clement.renaud@gmail.com>
2018-10-25 06:21:20 +0200

    force rerender Status component on multiple search requests

--- ui/app/pages/Home.jsx
+++ ui/app/pages/Home.jsx
@@ -136,7 +136,7 @@ class Home extends Component {
                     />
                   : <Status
                     hash={hash}
-
+                    key={hash}
                     />
                 }
             </div>
